c433c81d62d7d7c173e0accbaeabc8f121a46cf8
Hatula <shalev280804@gmail.com>
2022-01-02 10:48:59 -0500

    It now works pretty consistently

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import random
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+RESET = '\033[0m'
+
 words = set([word.strip() for word in open('./words.txt', 'r').readlines()])
 
 def update_results(word, greens, yellows, blacks):
@@ -21,31 +25,39 @@ def choose_next(greens, yellows, blacks):
 	potential_answers = set()
 	for word in list(words):
 		if any(char in word for char in blacks):
-			# print(f'black in word {word}')
 			continue
-
-		for yellow in yellows:
-			if yellow['char'] in word:
-				potential_answers.add(word)	
-			# if word[yellow['location']] == yellow['char']:
-			#	continue
-		if all(word[green['location']] == green['char'] for green in greens):
+		if not any(char in word for char in blacks) and all(yellow['char'] in word and word[yellow['location']] != yellow['char'] for yellow in yellows) and all(word[green['location']] == green['char'] for green in greens):
 			potential_answers.add(word)
-		elif word in potential_answers:
-
-			potential_answers.remove(word)
 	return potential_answers
 
+def color_word(word, greens, yellows):
+	res = ''
+	for index, char in enumerate(word):
+		if any(green['location'] == index and char == green['char'] for green in greens):
+			res += GREEN
+		elif any(yellow['char'] == char and index != yellow['location'] for yellow in yellows):
+			res += YELLOW
+		else:
+			res += RESET
+		res += char
+		 
+	return res + RESET
+
+
 def main():
+	tries = 1
 	word = random.choice(list(words))
+	while len(set(list(word))) < len(word):
+		word = random.choice(list(words))
 	greens = []
 	yellows = []
 	blacks = []
 	while True:
-		print(f'Word chosen: {word}')
+		print(f'{tries}. Word chosen: {color_word(word, greens, yellows)}')
+		tries += 1
 		res = update_results(word, greens, yellows, blacks)
 		if res:
-			print('Word found successfully')
+			print(f'{GREEN}Word found successfully{RESET}')
 			return 0
 		else:
 			words.remove(word)
